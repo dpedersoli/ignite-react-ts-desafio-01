55aa1f3f0e4183a6abe22892021441c17fc31715
dpedersoli <dpmsengineer@gmail.com>
2023-05-13 09:25:45 -0300

    few style changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,19 @@ import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react'
 import { Header } from './Components/Header'
 import { Task } from './Components/Task'
 
-import {Notepad, PlusCircle} from 'phosphor-react'
+import { Notepad, PlusCircle } from 'phosphor-react'
 
 import styles from './App.module.css'
 import './global.css'
 
 export function App() {
   const [tasks, setTasks] = useState([])
-  const [ newTaskText, setNewTaskText] = useState('');
+  const [newTaskText, setNewTaskText] = useState('');
 
   function handleCreateNewTask(event: FormEvent) {
     event.preventDefault()
 
-    setTasks([...tasks, newTaskText ])
+    setTasks([...tasks, newTaskText])
 
     setNewTaskText('')
   }
@@ -32,7 +32,7 @@ export function App() {
   return (
     <>
       <header>
-        <Header/>
+        <Header />
       </header>
 
       <main className={styles.main}>
@@ -48,9 +48,9 @@ export function App() {
           />
           <button
             type="submit"
-          > 
+          >
             Criar
-            <PlusCircle size={18}/>
+            <PlusCircle size={18} />
           </button>
         </form>
 
@@ -62,26 +62,35 @@ export function App() {
 
 
         {
-          tasks.length > 0 ? 
-          <div className={styles.tasksTable}>
-          {tasks.map(task => {
-            return (
-              <Task
-              taskContent={task}
-            />
-            )
-          })}
-          </div> :
-          <div className={styles.emptyTasksTable}>
-            <Notepad className={styles.img} size={56}/>
-            <p>Você ainda não tem tarefas cadastradas</p>
-            <p>Crie tarefas e organize seus itens a fazer</p>
-          </div>
+          tasks.length > 0 ?
+            <div className={styles.tasksTable}>
+              {tasks.map(task => {
+                return (
+                  <Task
+                    taskContent={task}
+                  />
+                )
+              })}
+            </div> :
+            <div className={styles.emptyTasksTable}>
+              <Notepad className={styles.img} size={56} />
+              <p>Você ainda não tem tarefas cadastradas</p>
+              <p>Crie tarefas e organize seus itens a fazer</p>
+            </div>
         }
 
 
-        
+
       </main>
     </>
   )
 }
+
+// 1. crio uma array de conteúdos(p) das tasks com valor inicial[]
+// 2. valor do input p / dentro do array de task que terá o conteúdo das task
+// 3. dou um map nas tasks
+// 4. se array de task > 0 mostra as tasks, se não, mostra a tela estilizada sem tarefa
+
+// 5. contador de tarefas
+
+// 6. excluir tarefas
