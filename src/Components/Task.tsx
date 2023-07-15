a91378236254ddc8049f98987a32c040c8fa32e1
dpedersoli <dpmsengineer@gmail.com>
2023-07-15 16:33:40 -0300

    paused

--- src/Components/Task.tsx
+++ src/Components/Task.tsx
@@ -1,10 +1,10 @@
-import {useState} from 'react'
+import { useState } from 'react'
 
 import { CheckCircle, Circle, Trash } from 'phosphor-react'
 import styles from './Task.module.css'
 
 interface TaskProps {
-  //// id: number -> "how to change the property value of only one item of an array through a component using react with typescript"
+  /// id: number -> "how to change the property value of only one item of an array through a component using react with typescript"
   taskContent: string;
   onDeleteTask: (task: string) => void
   onTaskCount: (task: boolean) => void
@@ -12,7 +12,7 @@ interface TaskProps {
   onTaskStatusChange: (newValue: string) => void
 }
 
-export function Task({taskContent, onDeleteTask, onTaskCount, taskStatus, onTaskStatusChange}: TaskProps) {
+export function Task({ taskContent, onDeleteTask, onTaskCount, taskStatus, onTaskStatusChange }: TaskProps) {
   const [isTaskDone, setTaskDone] = useState(false);
 
 
@@ -20,14 +20,14 @@ export function Task({taskContent, onDeleteTask, onTaskCount, taskStatus, onTask
     onDeleteTask(taskContent)
   }
 
-  function handleTaskCheck(){
+  function handleTaskCheck() {
     setTaskDone(!isTaskDone);
     onTaskCount(isTaskDone);
 
     //isso precisa ir para o chamamento do componente
-    if(taskStatus == 'undone'){
+    if (taskStatus == 'undone') {
       onTaskStatusChange('done')
-    } else if(taskStatus == 'done'){
+    } else if (taskStatus == 'done') {
       onTaskStatusChange('undone')
     }
     //isso precisa ir para o chamamento do componente
@@ -35,32 +35,32 @@ export function Task({taskContent, onDeleteTask, onTaskCount, taskStatus, onTask
 
   return (
     <div className={styles.task}>
-        <div className={styles.undoneTask}>
-          { taskStatus == 'undone' &&
-              <>
-                <Circle
-                  className={styles.undoneCircle}
-                  onClick={handleTaskCheck}
-                  size={24}
-                  />
-                <p>{taskContent}</p>
-              </>
-            }
-            { taskStatus == 'done' &&
-              <>
-                <CheckCircle
-                  className={styles.doneCircle}
-                  onClick={handleTaskCheck}
-                  size={24}
-                  weight="fill"
-                />
-                <p className={styles.doneTask}> {taskContent} </p>
-              </>
-            }
-          <button onClick={handleDeleteTask} title="deletar tarefa">
-            <Trash size={14} className={styles.trash}/>
-          </button>
-        </div>
+      <div className={styles.undoneTask}>
+        {taskStatus == 'undone' &&
+          <>
+            <Circle
+              className={styles.undoneCircle}
+              onClick={handleTaskCheck}
+              size={24}
+            />
+            <p>{taskContent}</p>
+          </>
+        }
+        {taskStatus == 'done' &&
+          <>
+            <CheckCircle
+              className={styles.doneCircle}
+              onClick={handleTaskCheck}
+              size={24}
+              weight="fill"
+            />
+            <p className={styles.doneTask}> {taskContent} </p>
+          </>
+        }
+        <button onClick={handleDeleteTask} title="deletar tarefa">
+          <Trash size={14} className={styles.trash} />
+        </button>
+      </div>
     </div>
   )
 }
